73b6babcee5f31d3e4b91d54fa2900c35a259a1f
coder6622 <tranngochoanglong469@gmail.com>
2023-02-25 16:17:32 +0700

    Create database - succeed 😋 😛 😝
    Tạo cơ sở dữ liệu thành công: các lớp và mapping, dbcontext  😋 😛 😝

---
+++ src/TipsAndTricks/TatBlog.Data/Mappings/AuthorMap.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Data.Mappings
+{
+  public class AuthorMap : IEntityTypeConfiguration<Author>
+  {
+    public void Configure(EntityTypeBuilder<Author> builder)
+    {
+      builder.ToTable("Authors");
+
+      builder.HasKey(a => a.Id);
+      builder.Property(a => a.FullName)
+        .HasMaxLength(100)
+        .IsRequired();
+
+      builder.Property(a => a.UrlSlug)
+        .HasMaxLength(500);
+
+      builder.Property(a => a.Email)
+        .HasMaxLength(150);
+
+      builder.Property(a => a.JoinedDate)
+        .HasColumnType("datetime");
+
+      builder.Property(a => a.Notes)
+        .HasMaxLength(500);
+    }
+  }
+}
