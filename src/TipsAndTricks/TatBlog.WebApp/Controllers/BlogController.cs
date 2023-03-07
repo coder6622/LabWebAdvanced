9cc3d1f3236c6a9c6842c8ec0569cd84f24e7c96
coder6622 <tranngochoanglong469@gmail.com>
2023-03-07 16:08:01 +0700

    Update Lab02 - B.4 to B.7 - create View and Controller base 4️⃣5️⃣6️⃣7️⃣✅
    Create View: _Header & _Sidebar, Blog Controller and View: Index, About, Contact ✅🆗

---
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TatBlog.WebApp.Controllers
+{
+  public class BlogController : Controller
+  {
+    public IActionResult Index()
+    {
+      ViewBag.CurrentTime = DateTime.Now
+        .ToString("HH:mm:ss");
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      return View();
+    }
+
+    public IActionResult Contact()
+    {
+      return View();
+    }
+
+    public IActionResult Rss()
+    {
+      return Content("Nội dung sẽ được cập nhật");
+    }
+  }
+}
--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -1,6 +1,30 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+{
+  builder.Services.AddControllersWithViews();
+}
 var app = builder.Build();
+{
+  if (app.Environment.IsDevelopment())
+  {
+    app.UseDeveloperExceptionPage();
+  }
+  else
+  {
+    app.UseExceptionHandler("/Blog/Error");
 
-app.MapGet("/", () => "Hello World!");
+    // hsts ??
+    app.UseHsts();
+  }
+
+  app.UseHttpsRedirection();
+
+  app.UseStaticFiles();
+
+  app.UseRouting();
+
+  app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Blog}/{action=Index}/{id?}");
+}
 
 app.Run();
