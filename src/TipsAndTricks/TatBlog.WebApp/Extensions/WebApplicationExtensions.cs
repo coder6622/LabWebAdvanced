2f7db86b41d15af431e745935c0f148760579839
coder6622 <tranngochoanglong469@gmail.com>
2023-03-07 23:13:07 +0700

    Lean program 2.B.14 - Done Lab02 Homework  ✅✅✔️🚫🚫

---
+++ src/TipsAndTricks/TatBlog.WebApp/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Extensions
+{
+  public static class WebApplicationExtensions
+  {
+    public static WebApplicationBuilder ConfigureMvc(
+      this WebApplicationBuilder builder)
+    {
+      builder.Services.AddControllersWithViews();
+      builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+      builder.Services.AddResponseCompression();
+      return builder;
+    }
+
+    public static WebApplicationBuilder ConfigureServices(
+      this WebApplicationBuilder builder)
+    {
+      builder.Services.AddDbContext<BlogDbContext>(options =>
+        options.UseSqlServer(
+          builder.Configuration
+            .GetConnectionString("DefaultConnection")));
+
+      builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+      builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+      return builder;
+    }
+
+    public static WebApplication UseRequestPipeline(
+      this WebApplication app)
+    {
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Blog/Error");
+
+        // thêm header Strict-Transport-Security vào http response
+        app.UseHsts();
+      }
+
+      // tự động nén http response
+      app.UseResponseCompression();
+
+      // chuyển hướng http sang https
+      app.UseHttpsRedirection();
+
+      app.UseStaticFiles();
+
+      // lựa chọn endpoint phù hợp để xử lý http request
+      app.UseRouting();
+      return app;
+    }
+
+    // thêm dữ liệu mẫu vào csdl
+    public static IApplicationBuilder UseDataSeeder(
+      this IApplicationBuilder app)
+    {
+      using var scope = app.ApplicationServices.CreateScope();
+      try
+      {
+        scope.ServiceProvider
+          .GetRequiredService<IDataSeeder>()
+          .Initialize();
+      }
+      catch (Exception ex)
+      {
+        scope.ServiceProvider
+            .GetRequiredService<ILogger<Program>>()
+            .LogError(ex, "Could not insert data into database");
+      }
+      return app;
+    }
+  }
+}
--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -3,64 +3,19 @@ using Microsoft.Identity.Client;
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
+using TatBlog.WebApp.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-  builder.Services.AddControllersWithViews();
-
-  builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-
-  builder.Services.AddDbContext<BlogDbContext>(options =>
-      options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
-
-  builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-  builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+  builder
+    .ConfigureMvc()
+    .ConfigureServices();
 }
 var app = builder.Build();
 {
-  if (app.Environment.IsDevelopment())
-  {
-    app.UseDeveloperExceptionPage();
-  }
-  else
-  {
-    app.UseExceptionHandler("/Blog/Error");
-
-    // hsts ??
-    app.UseHsts();
-  }
-
-  app.UseHttpsRedirection();
-
-  app.UseStaticFiles();
-
-  app.UseRouting();
-
-  app.MapControllerRoute(
-    name: "posts-by-category",
-    pattern: "blog/category/{slug}",
-    defaults: new { controller = "Blog", action = "Category" });
-
-  app.MapControllerRoute(
-    name: "posts-by-tag",
-    pattern: "blog/tag/{slug}",
-    defaults: new { controller = "Blog", action = "Tag" });
-
-  app.MapControllerRoute(
-    name: "single-post",
-    pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
-    defaults: new { controller = "Blog", action = "Post" });
-
-  app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Blog}/{action=Index}/{id?}");
-}
-
-using (var scope = app.Services.CreateScope())
-{
-  var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-  seeder.Initialize();
+  app.UseRequestPipeline();
+  app.UseBlogRoutes();
+  app.UseDataSeeder();
 }
 
 app.Run();
