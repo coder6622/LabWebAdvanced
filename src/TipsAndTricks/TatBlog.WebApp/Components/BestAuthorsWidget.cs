b0d53fbb791de4aec3c5060960c7a2cc05fe309a
coder6622 <tranngochoanglong469@gmail.com>
2023-03-10 18:18:23 +0700

    Update 2.C.3.d - Display top author ✔️🔅🔆🛂

---
+++ src/TipsAndTricks/TatBlog.WebApp/Components/BestAuthorsWidget.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Core;
+using TatBlog.Core.Contracts;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Components
+{
+  public class BestAuthorsWidget : ViewComponent
+  {
+    IAuthorRepository _authorRepository;
+
+    public BestAuthorsWidget(IAuthorRepository authorRepository)
+    {
+      _authorRepository = authorRepository;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+      IPagingParams pagingParams = new PagingParams()
+      {
+        PageNumber = 1,
+        PageSize = 4
+      };
+
+      var authors = await _authorRepository
+        .GetNAuthorTopPosts(4, pagingParams);
+
+      return View(authors);
+    }
+
+  }
+}
